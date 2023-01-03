ea5378d5136a21bec46a1d1f05dcb8fa29acca30
l00pinfinity <collinskipkemboi001@gmail.com>
2023-01-03 18:26:19 +0300

    remove unnecessary files

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import * as express from "express";
-
-const app = express();
-
-app.use('/', (req: express.Request, res: express.Response) => res.send("It contains my blog posts online, do you have any suggestions? A correction, perhaps? Make a pull request on Github or open an issue"));
-app.listen(3000, ()=> console.log("Listening on http://localhost:3000"));
